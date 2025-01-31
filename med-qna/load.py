47a6af7d6c2fc42df75308f9dd6c0123ee995cc0
Lim Jia Xian <limjiaxian2@gmail.com>
2025-01-31 22:54:57 +0800

    Updated pipeline for med42 model

--- med-qna/load.py
+++ med-qna/load.py
@@ -1,5 +1,5 @@
 # Load Med42 Llama3 model directly using transformer
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
+# Tokenizer and Model
 tokenizer = AutoTokenizer.from_pretrained("m42-health/Llama3-Med42-8B")
 model = AutoModelForCausalLM.from_pretrained("m42-health/Llama3-Med42-8B")
