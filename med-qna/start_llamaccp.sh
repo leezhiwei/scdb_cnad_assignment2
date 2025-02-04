fae0349e0643ca13fb9bb389c8ee55a86be06338
Lim Jia Xian <limjiaxian2@gmail.com>
2025-02-04 23:43:41 +0800

    Added curl with system role guidelines

--- med-qna/start_llamaccp.sh
+++ med-qna/start_llamaccp.sh
@@ -10,3 +10,5 @@ screen -s llama_med42_server_session
 llama-server -m models/Llama3-Med42-8B-Q5_K_M.gguf -c 2048 --host 0.0.0.0 --port 8000 -ngl 33
 # Testing with curl
 curl.exe -X POST http://192.168.2.108:8000/v1/chat/completions -H "Content-Type: applications/json" --data "{\"model\":\"m42-health/Llama3-Med42-8B\",\"messages\":[{\"role\":\"user\",\"content\":\"If I fall, what should I do?\"}]}"
+# Curl with system role to set guidelines on responses
+curl.exe -X POST http://192.168.2.108:8000/v1/chat/completions -H "Content-Type: application/json" --data "{\"model\":\"m42-health/Llama3-Med42-8B\",\"messages\":[{\"role\":\"system\",\"content\":\"Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don’t know the answer to a question, please don’t share false information.\"},{\"role\":\"user\",\"content\":\"If I fall, what should I do?\"}]}"
