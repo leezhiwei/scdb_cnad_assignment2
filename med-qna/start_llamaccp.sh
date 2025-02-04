2f45f42b5e88c7d445275079eda419e7b7942d9d
Lim Jia Xian <limjiaxian2@gmail.com>
2025-02-04 23:36:27 +0800

    Added screen session to shelll

--- med-qna/start_llamaccp.sh
+++ med-qna/start_llamaccp.sh
@@ -4,8 +4,9 @@ git clone https://github.com/ggerganov/llama.cpp
 cd llama.cpp
 # Compile the source to build llama.cpp
 make
+# Start a new screen session
+screen -s llama_med42_server_session
 # Start the Llama.cpp server to expose the model as an API with context length of 2048 tokens and port 8000
 llama-server -m models/Llama3-Med42-8B-Q5_K_M.gguf -c 2048 --host 0.0.0.0 --port 8000 -ngl 33
-
 # Testing with curl
 curl.exe -X POST http://192.168.2.108:8000/v1/chat/completions -H "Content-Type: applications/json" --data "{\"model\":\"m42-health/Llama3-Med42-8B\",\"messages\":[{\"role\":\"user\",\"content\":\"If I fall, what should I do?\"}]}"
