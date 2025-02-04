3d0928997e7f4b7926b230e939eb8dcc63100811
Lim Jia Xian <limjiaxian2@gmail.com>
2025-02-04 18:13:21 +0800

    Updated lamaccp

--- med-qna/startl_lamaccp.sh
+++ med-qna/startl_lamaccp.sh
@@ -6,3 +6,9 @@ cd llama.cpp
 make
 # Start the Llama.cpp server to expose the model as an API with context length of 2048 tokens and port 8000
 ./server -m med42/Llama3-med42.GGUF -c 2048 --host 0.0.0.0 --port 8000
+
+# Testing with curl
+curl --request POST \
+    --url http://localhost:8080/completion \
+    --header "Content-Type: application/json" \
+    --data '{"prompt": "If I fall, what should I do?","n_predict": 128}'
