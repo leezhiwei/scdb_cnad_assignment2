a6074a4d1efaa32ba8380d51faa466e8e0894e50
Lee Zhi Wei <zhiweilee.com@gmail.com>
2025-02-12 22:51:45 +0800

    Add HTTPS

--- video-server/main.go
+++ video-server/main.go
@@ -168,7 +168,7 @@ func main() {
 	})
 	router.POST("/api/v1/videoconf/join/:meetingID/:userID", joinMeeting)
 	router.GET("/api/v1/videoconf/peer/:meetingID/:userID", getPeerID)
-	router.Run(":8080")
+	router.RunTLS(":8080", "../certs/server.cert", "../certs/server.key")
 }
 
 // user is the caller of the method
