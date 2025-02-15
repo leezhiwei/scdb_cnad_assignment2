7766db9cb6e6e07c5ac5d30b84f6dbd30130c3b8
Lim Jia Xian <limjiaxian2@gmail.com>
2025-02-15 21:17:26 +0800

    Quantization 6 bit Med42

--- med-qna/start_llamaccp.sh
+++ med-qna/start_llamaccp.sh
@@ -6,8 +6,10 @@ cd llama.cpp
 make
 # Start a new screen session
 screen -s llama_med42_server_session
-# Start the Llama.cpp server to expose the model as an API with context length of 2048 tokens and port 8000
-llama-server -m models/Llama3-Med42-8B-Q5_K_M.gguf -c 2048 --host 0.0.0.0 --port 8000 -ngl 33
+# Start the Llama.cpp server to expose the Med42 Llama3 Q5 model as an API with context length of 2048 tokens and port 8000
+# llama-server -m models/Llama3-Med42-8B-Q5_K_M.gguf -c 2048 --host 0.0.0.0 --port 8000 -ngl 33
+# Start the Llama.cpp server to expose the Med42 Llama3 Q6 model as an API with context length of 2048 tokens and port 8000
+llama-server -m models/Llama3-Med42-8B-Q6_K_M.gguf -c 2048 --host 0.0.0.0 --port 8000 -ngl 33
 # Testing with curl
 curl.exe -X POST http://192.168.2.108:8000/v1/chat/completions -H "Content-Type: applications/json" --data "{\"model\":\"m42-health/Llama3-Med42-8B\",\"messages\":[{\"role\":\"user\",\"content\":\"If I fall, what should I do?\"}]}"
 # Curl with system role to set guidelines on responses
